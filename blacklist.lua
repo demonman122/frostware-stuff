ccc91b5f8cba6b7dbd1c3fa64d716d6138e6a09d
frosted <66559863+frost2w@users.noreply.github.com>
2022-01-09 12:58:51 +0000

    Add files via upload

---
+++ blacklist.lua
@@ -0,0 +1,30 @@
+kickmessage = "If you don't like my script, don't use it!"
+blacklist = {""}
+
+mains = {"frostedsWish"}
+
+special = {"alexxgravve", "IqramDaThugLife"}
+
+donators = {""}
+
+bigdonators = {""}
+
+nitroboosters = {"IqramDaThugLife"}
+
+hitlist = {""}
+
+ 
+-- Only people in the blacklist table are blacklisted.
+-- No one in any of these lists has the power to get you blacklisted from ANY of my scripts.
+ 
+--[[ 
+
+- Accounts in the blacklisted role belong to people who have proven they do not need or want to use the script. (Shit-talking the script while using it, calling the script and me garbage, etc)
+- Accounts in the main table belong to me
+- Accounts in the special table belong to friends of mine, or people who are important in the streets community (if you're not in this list, that doesn't mean you're not special omg)
+- Accounts in the donators role have donated around 30 robux to the script, and are very cool
+- Accounts in the bigdonators role have donated around 100 robux to the script, and are also very cool
+- Accounts in the nitroboosters role have boosted the server, and are very cool https://discord.gg/rfHpfCAfXN
+- Accounts in the hitlist role probably don't exploit, but have proved themselves to be enemies of the script, and those who use it.
+
+--]]
